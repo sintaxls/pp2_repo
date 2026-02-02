0c05dc375f16446641fd773ec7be6989fb5b7821
sintaxls <sintaxls@users.noreply.github.com>
2026-02-02 21:13:55 +0500

    add_boolean_operators

---
+++ boolean/boolean_operators.py
@@ -0,0 +1,31 @@
+# The following will return False:
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+# The following will return True:
+
+bool("hello")
+bool(1)
+bool([0])
+
+
+# Boolean operators
+
+# a = True
+# b = False
+
+# # and
+# print(a and b)
+
+# # or
+# print(a or b)
+
+# # not
+# print(not a)
