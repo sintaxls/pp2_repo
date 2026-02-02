be6ccd0de6a8e44fa4fdaa4cf2d36bc777314f16
sintaxls <sintaxls@users.noreply.github.com>
2026-02-02 21:16:01 +0500

    edit__boolean_operators

--- boolean/boolean_operators.py
+++ boolean/boolean_operators.py
@@ -29,3 +29,27 @@ bool([0])
 
 # # not
 # print(not a)
+
+
+
+# Test if a number is greater than 0 and less than 10:
+
+# x = 5
+
+# print(x > 0 and x < 10)
+
+
+
+# Test if a number is less than 5 or greater than 10:
+
+# x = 5
+
+# print(x < 5 or x > 10)
+
+
+
+# Reverse the result with not:
+
+# x = 5
+
+# print(not(x > 3 and x < 10))
