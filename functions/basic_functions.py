f15760b97173585d18ea650fd02f7480046b6bd2
sintaxls <sintaxls@users.noreply.github.com>
2026-02-09 22:28:05 +0500

    add basic_functions

---
+++ functions/basic_functions.py
@@ -0,0 +1,38 @@
+# example
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+# example 2
+# def my_function():
+#   print("Hello from a function")
+
+# my_function()
+# my_function()
+# my_function()
+
+
+# examle 3
+# Valid function names:
+
+# calculate_sum()
+# _private_function()
+# myFunction2()
+
+# example 4
+# def fahrenheit_to_celsius(fahrenheit):
+#   return (fahrenheit - 32) * 5 / 9
+
+# print(fahrenheit_to_celsius(77))
+# print(fahrenheit_to_celsius(95))
+# print(fahrenheit_to_celsius(50))
+
+#example 5
+# def get_greeting():
+#   return "Hello from a function"
+
+# message = get_greeting()
+# print(message)
+
