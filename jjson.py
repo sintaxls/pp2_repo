4c14032ecba53afc9f425414ca68002c546bb9dc
sintaxls <sintaxls@users.noreply.github.com>
2026-02-23 23:00:43 +0500

    add json

---
+++ jjson.py
@@ -0,0 +1,17 @@
+import json
+
+with open("sample-data.json") as f:
+    data = json.load(f)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':50} {'Speed':10} {'MTU':6}")
+print("-" * 80)
+
+for item in data["imdata"]:
+    att = item["l1PhysIf"]["attributes"]
+    dn = att["dn"]
+    speed = att["speed"]
+    mtu = att["mtu"]
+
+    print(f"{dn:50} {speed:10} {mtu:6}")
