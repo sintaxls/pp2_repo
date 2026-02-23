5b686c05ec73015acfa85f284c77872343ec0fb6
sintaxls <sintaxls@users.noreply.github.com>
2026-02-23 22:49:59 +0500

    add math

---
+++ mmath.py
@@ -0,0 +1,38 @@
+# 1. Write a Python program to convert degree to radian.
+# import math
+# dg = float(input("Input degree: "))
+
+# print("Output radian:", dg * math.pi / 180)
+
+
+
+# 2. Write a Python program to calculate the area of a trapezoid.
+
+# height = int(input("height: "))
+# base1 = int(input("base1: "))
+# base2 = int(input("base2: "))
+
+# a = 0.5 * height * (base1+base2)
+
+# print("Area:", a)
+
+
+
+# 3. Write a Python program to calculate the area of regular polygon.
+# import math
+# n = int(input("Input number of sides: "))
+# le = int(input("Input the length of a side: "))
+
+# a = (n * le**2) / (4 * math.tan(math.pi / n))
+
+# print("Area:", a)
+
+
+# 4. Write a Python program to calculate the area of a parallelogram.
+
+# b = int(input("Length of base: "))
+# h = int(input("Height of parallelogram: "))
+
+# a = b * h
+
+# print("Area of parallelogram:", float(a))
