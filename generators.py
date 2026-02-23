55dbb3f0b3d3f2f0faa4c1370f85297af61f97ba
sintaxls <sintaxls@users.noreply.github.com>
2026-02-23 21:51:34 +0500

    add task 3

--- generators.py
+++ generators.py
@@ -12,13 +12,26 @@
 
 # 2. Write a program using generator to print the even numbers between 0 and n in comma separated form where n is input from console.
 
-def evengenerator(n : int):
-    for i in range(1,n+1,2):
-        if i == n-1 or i == n:
-            print(i, end="")
-            break
+# def evengenerator(n : int):
+#     for i in range(1,n+1,2):
+#         if i == n-1 or i == n:
+#             print(i, end="")
+#             break
     
-        print(f"{i}, ",end="")
+#         print(f"{i}, ",end="")
+
+# n = int(input())
+# evengenerator(n)
+
+
+# 3. Define a function with a generator which can iterate the numbers, which are divisible by 3 and 4, between a given range 0 and n.
+
+def iteratediv34(n:int):
+    a = []
+    for i in range(3,n+1):
+        if i%3==0 and i%4==0:
+            a.append(i)
+    print(",".join(map(str, a)))
 
 n = int(input())
-evengenerator(n)
+iteratediv34(n)
