1874fa0917d92b34220018852da20844cc92c31b
sintaxls <sintaxls@users.noreply.github.com>
2026-03-09 18:24:24 +0500

    add dir management

---
+++ directory_management/move_files.py
@@ -0,0 +1,11 @@
+# example 1
+# from pathlib import Path
+# import shutil
+
+# filetomove = "moveme1.txt"
+# source = Path(f"directory_management/move_from/{filetomove}")
+# destination = Path(f"directory_management/move_to/{filetomove}")
+
+# shutil.move(str(source), str(destination))
+# print("File moved")
+
