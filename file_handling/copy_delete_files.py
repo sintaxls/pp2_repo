d2fc733c1dd43bc6d1693739f1300c5e025e8dc6
sintaxls <sintaxls@users.noreply.github.com>
2026-03-09 17:55:00 +0500

    add file handling

---
+++ file_handling/copy_delete_files.py
@@ -0,0 +1,16 @@
+# example 1
+# with open("file_handling/examplefile.txt", "w") as f:
+#   f.write("Woops! I have deleted the content!")
+
+# with open("file_handling/examplefile.txt") as f:
+#   print(f.read())
+
+
+# example 2
+# import os
+# os.remove("file_handling/deleteme.txt")
+
+# example 3
+# import os
+# os.rmdir("file_handling/deleteme")
+
--- file_handling/read_files.py
+++ file_handling/read_files.py
@@ -1,5 +1,20 @@
 # example 1
-# f = open("file_handling/examplefile.txt")
+# f = open("examplefile1.txt")
+# print(f.read())
 
 # example 2
 # f = open("file_handling/examplefile.txt", "rt")
+# print(f.read())
+
+# example 3
+# with open("examplefile1.txt") as f:
+#   print(f.read())
+
+# example 4
+# f = open("examplefile1.txt")
+# print(f.readline())
+# f.close()
+
+# example 5
+# with open("examplefile1.txt") as f:
+#   print(f.read(3))
