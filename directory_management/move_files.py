e37b799cfb194df2e274bdc6070cd820a58af2a8
sintaxls <sintaxls@users.noreply.github.com>
2026-03-09 18:52:01 +0500

    add create list dirs

--- directory_management/move_files.py
+++ directory_management/move_files.py
@@ -1,4 +1,4 @@
-# example 1
+# example 1 move
 # from pathlib import Path
 # import shutil
 
@@ -7,5 +7,15 @@
 # destination = Path(f"directory_management/move_to/{filetomove}")
 
 # shutil.move(str(source), str(destination))
-# print("File moved")
+# print("file moved")
 
+# example 2 copy
+# from pathlib import Path
+# import shutil
+
+# filetomove = "moveme1.txt"
+# source = Path(f"directory_management/move_from/{filetomove}")
+# destination = Path(f"directory_management/move_to/{filetomove}")
+
+# shutil.copy(str(source), str(destination))
+# print("file copied")
